aeec81b911891bc0f8e755a51da527bbe0bae83f
Sujin1517 <11tnwls12@gmail.com>
2024-05-21 11:49:56 +0900

    change db to mysql > h2

--- src/test/java/com/example/test_back/TestBackApplicationTests.java
+++ src/test/java/com/example/test_back/TestBackApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.test_back;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class TestBackApplicationTests {
@@ -10,4 +14,13 @@ class TestBackApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void dbNameIsH2(@Value("${spring.datasource.url}") String url) {
+		assertEquals("h2", url.split(":")[1]);
+	}
+
+	@Test
+	void dbUserIsSa(@Value("${spring.datasource.username}") String username) {
+		assertEquals("sa", username);
+	}
 }
